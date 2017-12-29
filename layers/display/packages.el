f61b92b5cadb96f8ba4dd14b798ce11bad9689da
alldigital <edward@tulupnikov.com>
2017-12-29 15:37:10 +0300

    Switch to variable font in org-mode. Switch to Fira Mono

--- layers/display/packages.el
+++ layers/display/packages.el
@@ -30,6 +30,64 @@
 ;; material colors for pdf-view night mode
 (setq pdf-view-midnight-colors '("#FFFFFF" . "#263238" ))
 
+(setq ed/original-height 105)
+
+(defun ed/setup-fonts ()
+  ;; default font and variable-pitch fonts
+  (set-face-attribute 'default nil
+                      :family "Fira Mono"
+                      :height ed/original-height)
+  (dolist (face '(mode-line mode-line-inactive minibuffer-prompt))
+    (set-face-attribute face nil :family "Fira Mono"
+                        :height ed/original-height))
+  (set-face-attribute 'variable-pitch nil
+                      :family "Fira Sans" :height ed/original-height)
+  (set-face-attribute 'fixed-pitch nil
+                      :family "Fira Mono" :height ed/original-height)
+  ;; font for all unicode characters
+  ;;(set-fontset-font t 'unicode "DejaVu Sans Mono" nil 'prepend)
+  )
+
+(add-hook 'after-init-hook #'ed/setup-fonts)
+
+(setq ed/variable-org-enabled t)
+
+(when ed/variable-org-enabled
+  (add-hook 'org-mode-hook 'variable-pitch-mode)
+  (add-hook 'markdown-mode-hook 'variable-pitch-mode)
+
+  (defun ed/adjoin-to-list-or-symbol (element list-or-symbol)
+    (let ((list (if (not (listp list-or-symbol))
+                    (list list-or-symbol)
+                  list-or-symbol)))
+      (require 'cl-lib)
+      (cl-adjoin element list)))
+
+  ;; Fontify certain org things with fixed-width
+  (eval-after-load "org"
+    '(mapc
+      (lambda (face)
+        (set-face-attribute
+         face nil
+         :inherit
+         (ed/adjoin-to-list-or-symbol
+          'fixed-pitch
+          (face-attribute face :inherit))))
+      (list 'org-code 'org-block 'org-table
+            'org-verbatim 'org-formula 'org-macro)))
+
+  ;; Fontify certain markdown things with fixed-width
+  (eval-after-load "markdown-mode"
+    '(mapc
+      (lambda (face)
+        (set-face-attribute
+         face nil
+         :inherit
+         (ed/adjoin-to-list-or-symbol
+          'fixed-pitch
+          (face-attribute face :inherit))))
+      (list 'markdown-pre-face 'markdown-inline-code-face))))
+
 (setq custom-safe-themes t)
 (load-theme 'material)
 
